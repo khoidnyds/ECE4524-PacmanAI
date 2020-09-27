4066980adb1c08039b0851d8cf55f9cc04d80578
khoidnyds <khoidnyds@vt.edu>
2020-09-27 00:54:37 -0400

    alpha-beta prune

--- p2/multiagent/multiAgents.py
+++ p2/multiagent/multiAgents.py
@@ -1,3 +1,4 @@
+import sys
 # multiAgents.py
 # --------------
 # Licensing Information:  You are free to use or extend these projects for
@@ -73,8 +74,38 @@ class ReflexAgent(Agent):
         newGhostStates = successorGameState.getGhostStates()
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
-        "*** YOUR CODE HERE ***"
-        return successorGameState.getScore()
+        "*** YOUR CODE HERE ***" 
+        # Get possible locations of ghost in next State
+        ghosts = []
+        for ghost in newGhostStates:
+            gx, gy = ghost.configuration.getPosition()
+            for i, j in [(0,0),(0,1),(1,0),(-1,0),(0,-1)]:
+                ghosts.append((gx+i, gy+j))
+
+        # Get new pacman position and food locations    
+        current_food = currentGameState.getFood()
+        x, y = newPos
+        w, h = currentGameState.data.layout.width, currentGameState.data.layout.height
+ 
+        # return fail value when ghost catch pacman
+        if (x, y) in ghosts:
+            return -1
+        # return reward for food when pacman eat food
+        if current_food[x][y]:
+            return sys.maxsize
+
+        # evaluate all food locations in the map by Manhantan distance, the farther the less points
+        food_score = []
+        for a in range(1, w - 1):
+            for b in range(1, h - 1):
+                if current_food[a][b]:
+                    distance = abs(x - a) + abs(y - b)
+                    food_score.append(distance)
+ 
+        if food_score:
+            return w + h - min(food_score) # max score is (w + h) which is maximum score can get from the Manhantan distance
+        else:
+            return 0
 
 def scoreEvaluationFunction(currentGameState):
     """
@@ -135,8 +166,35 @@ class MinimaxAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
+        return self.MinimaxSearch(gameState, current_depth=1, agentIndex=0)
+        
+    def MinimaxSearch(self, gameState, current_depth, agentIndex):
+        # stop when reach the depth or win or lose
+        if current_depth > self.depth or gameState.isWin() or gameState.isLose():
+            return self.evaluationFunction(gameState)
+        
+        # next possible moves
+        next_moves = [action for action in gameState.getLegalActions(agentIndex) if action is not 'Stop']
+        
+        # update next depth and next agent
+        next_agent = agentIndex + 1 # pacman -> ghost_1 -> ghost_2 -> ...
+        next_depth = current_depth
+        if next_agent >= gameState.getNumAgents():
+            next_agent = 0
+            next_depth += 1
+        
+        # Evaluate next moves having maximum score
+        moves_scores = [self.MinimaxSearch(gameState.generateSuccessor(agentIndex, action), next_depth, next_agent) for action in next_moves]
+        
+        # decide the next move here
+        if agentIndex == 0 and current_depth == 1: # at root
+            return next_moves[moves_scores.index(max(moves_scores))]
+        
+        # min for ghost, max for pacman
+        if agentIndex:
+            return min(moves_scores)
+        return max(moves_scores)
+            
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
     Your minimax agent with alpha-beta pruning (question 3)
@@ -147,7 +205,51 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.AlphaBeta(gameState, current_depth=1, agentIndex=0, alpha=-sys.maxsize, beta=sys.maxsize, value = -sys.maxsize)
+    
+    def AlphaBeta(self, gameState, current_depth, agentIndex, alpha, beta, value):
+        # stop when reach the depth or win or lose
+        if current_depth > self.depth or gameState.isWin() or gameState.isLose():
+            return self.evaluationFunction(gameState)
+        
+        # next possible moves
+        next_moves = [action for action in gameState.getLegalActions(agentIndex) if action is not 'Stop']
+        
+        # update next depth and next agent
+        next_agent = agentIndex + 1 # pacman -> ghost_1 -> ghost_2 -> ...
+        next_depth = current_depth
+        if next_agent >= gameState.getNumAgents():
+            next_agent = 0
+            next_depth += 1
+        
+        # decide the next move here
+        if agentIndex == 0 and current_depth == 1: # at root
+            scores = []
+            for action in next_moves:
+                value = self.AlphaBeta(gameState.generateSuccessor(agentIndex, action), next_depth, next_agent, alpha, beta, value)
+                scores.append(alpha)
+                if value > beta:
+                    return next_moves[scores.index(max(scores))]
+            return next_moves[scores.index(max(scores))]
+        
+        # update alpha, beta
+        if agentIndex:
+            value = beta
+            for action in next_moves:
+                beta = self.AlphaBeta(gameState.generateSuccessor(agentIndex, action), next_depth, next_agent, alpha, beta, value)
+                value = min(value, beta)
+                if alpha > value: # prune
+                    return value
+                beta = min(beta, value)
+            return value
+        
+        value = alpha
+        for action in next_moves:
+            value = max(value, self.AlphaBeta(gameState.generateSuccessor(agentIndex, action), next_depth, next_agent, alpha, beta, value))
+            if beta < value: # prune
+                return value
+            alpha = max(alpha, value)
+        return value
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
@@ -162,7 +264,42 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         legal moves.
         """
         "*** YOUR CODE HERE ***"
+        return self.ExpectiMax(gameState, 1, 0)
         util.raiseNotDefined()
+
+    def ExpectiMax(self, gameState, currentDepth, agentIndex):
+        "terminal check"
+        if currentDepth > self.depth or gameState.isWin() or gameState.isLose():
+            return self.evaluationFunction(gameState)
+        
+        "expectimax algorithm"
+        legalMoves = [action for action in gameState.getLegalActions(agentIndex) if action!='Stop']
+        
+        # update next depth
+        nextIndex = agentIndex + 1
+        nextDepth = currentDepth
+        if nextIndex >= gameState.getNumAgents():
+            nextIndex = 0
+            nextDepth += 1
+        
+        results = [self.ExpectiMax( gameState.generateSuccessor(agentIndex, action) , nextDepth, nextIndex) for action in legalMoves]
+            
+        if agentIndex == 0 and currentDepth == 1: # pacman first move
+            bestMove = max(results)
+            bestIndices = [index for index in range(len(results)) if results[index] == bestMove]
+            chosenIndex = random.choice(bestIndices) # Pick randomly among the best
+            #print 'pacman %d' % bestMove
+            return legalMoves[chosenIndex]
+        
+        if agentIndex == 0:
+            bestMove = max(results)
+            #print bestMove
+            return bestMove
+        else:
+            "In ghost node, return the average(expected) value of action"
+            bestMove = sum(results)/len(results)
+            #print bestMove, sum(results), len(results)
+            return bestMove
 
 def betterEvaluationFunction(currentGameState):
     """
@@ -172,6 +309,98 @@ def betterEvaluationFunction(currentGameState):
     DESCRIPTION: <write something here so we know what you did>
     """
     "*** YOUR CODE HERE ***"
+    if currentGameState.isWin() :  return sys.maxsize
+    if currentGameState.isLose() :  return -sys.maxsize
+    
+    currentPos = currentGameState.getPacmanPosition()
+    currentFood = currentGameState.getFood()
+    GhostStates = currentGameState.getGhostStates()
+    capsulePos = currentGameState.getCapsules()
+    
+    weightFood, weightGhost, weightCapsule, weightHunter = 5.0, 5.0, 5.0, 0.0
+    ghostScore, capsuleScore, hunterScore = 0.0, 0.0, 0.0
+    #print '\nnewPos '+str(newPos)
+    #print 'oldFood '+str(oldFood.asList())
+    #print "score %d" % successorGameState.getScore()
+    
+    "obtain food score" # may closestFood be zero?
+    currentFoodList = currentFood.asList()
+    closestFood = min([util.manhattanDistance(currentPos, foodPos) for foodPos in currentFoodList])
+    foodScore = 1.0 / closestFood
+    
+    "obtain ghost, capsule, hunting score"
+    if GhostStates:
+        ghostPositions = [ghostState.getPosition() for ghostState in GhostStates]
+        ScaredTimes = [ghostState.scaredTimer for ghostState in GhostStates]
+        ghostDistances = [util.manhattanDistance(currentPos, ghostPos) for ghostPos in ghostPositions]
+        
+        if sum(ScaredTimes) == 0 : # escape and eat mode
+            closestGhost = min(ghostDistances)
+            ghostCenterPos = ( sum([ghostPos[0] for ghostPos in ghostPositions])/len(GhostStates),\
+                               sum([ghostPos[1] for ghostPos in ghostPositions])/len(GhostStates))
+            ghostCenterDist = util.manhattanDistance(currentPos, ghostCenterPos)
+            #print 'center ' + str(ghostCenterPos)
+            if ghostCenterDist <= closestGhost and closestGhost >= 1 and closestGhost <= 5:
+                if len(capsulePos) != 0:
+                    closestCapsule = min([util.manhattanDistance(capsule,currentPos) for capsule in capsulePos])
+                    if closestCapsule <= 3:
+                        weightCapsule, capsuleScore = 20.0, (1.0 / closestCapsule)
+                        weightGhost, ghostScore = 3.0, (-1.0 / (ghostCenterDist+1))
+                    else:
+                        weightGhost, ghostScore = 10.0, (-1.0 / (ghostCenterDist+1))
+                else:
+                    weightGhost, ghostScore = 10.0, (-1.0 / (ghostCenterDist+1))
+            elif ghostCenterDist >= closestGhost and closestGhost >= 1 :
+                weightFood *= 2
+                if len(capsulePos) != 0:
+                    closestCapsule = min([util.manhattanDistance(capsule,currentPos) for capsule in capsulePos])
+                    if closestCapsule <= 3:
+                        weightCapsule, capsuleScore = 15.0, (1.0 / closestCapsule)
+                        weightGhost, ghostScore = 3.0, (-1.0 / closestGhost)
+                    else:
+                        ghostScore = -1.0 / closestGhost
+                else:
+                    ghostScore = -1.0 / closestGhost
+            elif closestGhost == 0:
+                return -sys.maxsize
+            elif closestGhost == 1:
+                weightGhost, ghostScore = 15.0, (-1.0 / closestGhost)
+            else:
+                ghostScore = -1.0 / closestGhost
+        else: # hunter mode
+            normalGhostDist = []
+            closestPrey = sys.maxsize
+            ghostCenterX, ghostCenterY = 0.0, 0.0
+            for (index, ghostDist) in enumerate(ghostDistances):
+                if ScaredTimes[index] == 0 :
+                    normalGhostDist.append(ghostDist)
+                    ghostCenterX += ghostPositions[index][0]
+                    ghostCenterY += ghostPositions[index][1]
+                else:
+                    if ghostDist <= ScaredTimes[index] :
+                        if ghostDist < closestPrey:
+                            closestPrey = ghostDistances[index]
+            if normalGhostDist:
+                closestGhost = min(normalGhostDist)
+                ghostCenterPos = ( ghostCenterX/len(normalGhostDist), ghostCenterY/len(normalGhostDist))
+                ghostCenterDist = util.manhattanDistance(currentPos, ghostCenterPos)
+                if ghostCenterDist <= closestGhost and closestGhost >= 1 and closestGhost <= 5:
+                    weightGhost, ghostScore = 10.0, (- 1.0 / (ghostCenterDist+1))
+                elif ghostCenterDist >= closestGhost and closestGhost >= 1 :
+                    ghostScore = -1.0 / closestGhost
+                elif closestGhost == 0:
+                    return -sys.maxsize
+                elif closestGhost == 1:
+                    weightGhost, ghostScore = 15.0, (-1.0 / closestGhost)
+                else:
+                    ghostScore = - 1.0 / closestGhost
+            weightHunter, hunterScore = 35.0, (1.0 / closestPrey)
+    
+    "a new evaluation function."
+    heuristic = currentGameState.getScore() + \
+                weightFood*foodScore + weightGhost*ghostScore + \
+                weightCapsule*capsuleScore + weightHunter*hunterScore
+    return heuristic
     util.raiseNotDefined()
 
 # Abbreviation
